f9f3b741c4dcf0949379fcc4fb60a97de85a6c62
DhirajS26 <120782459+DhirajS26@users.noreply.github.com>
2023-01-16 11:26:45 +0530

    Add files via upload

---
+++ 1input.sh
@@ -0,0 +1,4 @@
+read -p "Enter Number A:" a
+read -p "Enter Number B:" b
+read -p "Enter Number C:" c
+
---
+++ 2.sh
@@ -0,0 +1,6 @@
+read -p "Enter Number A:" a
+read -p "Enter Number B:" b
+read -p "Enter Number C:" c
+
+answer1=$(($a + $b * $c))
+echo "$answer1"
